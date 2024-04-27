d9a9f8bd971574d5cdbd3e97ddff1b253f13566e
FabianaMFZ <fabianamfz@gmail.com>
2024-04-27 16:30:37 +0100

    'lab'

---
+++ lab_intro_sql.sql
@@ -0,0 +1,35 @@
+
+-- Instructions
+
+-- Use sakila database.
+USE SAKILA;
+
+-- Get all the data from tables actor, film and customer.
+SELECT *
+FROM ACTOR;
+
+SELECT *
+FROM FILM;
+
+SELECT *
+FROM CUSTOMER;
+
+-- Get film titles.
+SELECT TITLE
+FROM FILM;
+
+-- Get unique list of film languages under the alias language. 
+SELECT DISTINCT NAME AS LANGUAGES
+FROM LANGUAGE;
+   
+-- Find out how many stores the company has.
+SELECT COUNT(*)
+FROM STORE;
+
+-- Find out how many employees staff the company has.
+SELECT COUNT(*)
+FROM STAFF;
+
+-- Return a list of employees first names only.
+SELECT FIRST_NAME
+FROM STAFF;
