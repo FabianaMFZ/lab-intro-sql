008db5e41b9115911393269bd58787b35409bd38
FabianaMFZ <fabianamfz@gmail.com>
2024-04-27 16:38:37 +0100

    'lab'

--- lab_intro_sql.sql
+++ lab_intro_sql.sql
@@ -23,11 +23,11 @@ SELECT DISTINCT NAME AS LANGUAGES
 FROM LANGUAGE;
    
 -- Find out how many stores the company has.
-SELECT COUNT(*)
+SELECT COUNT(DISTINCT STORE_ID)
 FROM STORE;
 
 -- Find out how many employees staff the company has.
-SELECT COUNT(*)
+SELECT COUNT(DISTINCT STAFF_ID)
 FROM STAFF;
 
 -- Return a list of employees first names only.
